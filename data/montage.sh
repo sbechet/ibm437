90028b15331d9ff3a7b8bb35d17404c48baeba1a
Sebastien Bechet <sebastien.bechet@osinix.com>
2020-04-13 16:21:43 +0200

    add raw code

--- data/montage.sh
+++ data/montage.sh
@@ -11,3 +11,6 @@ rm -f font_8_8_normal_??.png
 magick montage -geometry +0+0 `ls font_8_8_bold_*.png | sort` font_8_8_bold.png
 rm -f font_8_8_bold_??.png
 
+convert font_9_14_normal.png -depth 1 gray:font_9_14_normal.raw
+convert font_8_8_normal.png -depth 1 gray:font_8_8_normal.raw
+convert font_8_8_bold.png -depth 1 gray:font_8_8_bold.raw
